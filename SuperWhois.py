b93a3f00c918efb049b35665eda5f33ecdf37d55
b3tar00t <74288865+b3tar00t@users.noreply.github.com>
2020-11-12 16:03:52 +0530

    Create SuperWhois.py

---
+++ SuperWhois.py
@@ -0,0 +1,45 @@
+print('')
+
+print('')
+
+print("\033[1;31;40m")
+
+print("*********************")
+
+print("*********************")
+
+print("*********************")
+
+#!/usr/bin/python2
+
+import os
+
+import sys
+
+os.system("figlet SuperWhois")
+
+print('')
+
+print('')
+
+print("Coded By b3ta_r00t")
+
+print("[-] Instagram = @b3ta_r00t ")
+
+print('')
+
+print('')
+
+url = raw_input("[+] Enter Website:")
+
+os.system("whois %s" % (url))
+
+print('')
+
+print('')
+
+print('------Thank You For using my Tool------')
+
+print('')
+
+print('')
